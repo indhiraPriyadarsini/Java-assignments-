2f015ed42a84b72558df8e80bf8bf81c34b74063
indhiraPriyadarsini <87574789+indhiraPriyadarsini@users.noreply.github.com>
2022-03-20 22:12:20 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,27 +1,41 @@
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
-class Main {
-  public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-    String str1 = input.nextLine();
-    String str2 = input.nextLine();;
-    str1 = str1.toLowerCase();
-    str2 = str2.toLowerCase();
-    if(str1.length() == str2.length()) {
-      char[] charArray1 = str1.toCharArray();
-      char[] charArray2 = str2.toCharArray();
-      Arrays.sort(charArray1);
-      Arrays.sort(charArray2);
-      boolean result = Arrays.equals(charArray1, charArray2);
-      if(result) {
-        System.out.println(str1 + " and " + str2 + " are anagram.");
-      }
-      else {
-        System.out.println(str1 + " and " + str2 + " are not anagram.");
-      }
-    }
-    else {
-      System.out.println(str1 + " and " + str2 + " are not anagram.");
-    }
-  }
+import com.sun.jdi.Value;
+public class anagramHashing {
+
+	public static void main(String[] args) {
+		int i,j;
+		Scanner input = new Scanner(System.in);
+		String str1 = input.nextLine();
+		String str2 = input.nextLine();
+		 HashMap < Character, Integer > hashmap1 = new HashMap < Character, Integer > ();
+		 HashMap < Character, Integer > hashmap2 = new HashMap < Character, Integer > ();
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		for(i=0; i< arr1.length; i++) {
+			if(hashmap1.get(arr1[i])== null) {
+				hashmap1.put(arr1[i],1);
+			}
+			else {
+				Integer c = (int) hashmap1.get(arr1[i]);
+				hashmap1.put(arr1[i], c++);
+			}
+		}
+		
+		for(j=0; j< arr2.length; j++) {
+			if(hashmap2.get(arr2[j])== null) {
+				hashmap2.put(arr2[j],1);
+			}
+			else {
+				Integer d = (int) hashmap2.get(arr2[j]);
+				hashmap2.put(arr2[j], d++);
+			}
+		}
+		if(hashmap1.equals(hashmap2))
+			System.out.println("anagram");
+		else
+			System.out.println("not anagram");
+
+	}
+
 }
