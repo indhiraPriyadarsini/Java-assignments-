b6e5c80f2e44972e80f96607e53761d2ec5749ec
indhiraPriyadarsini <87574789+indhiraPriyadarsini@users.noreply.github.com>
2022-03-30 15:18:44 +0530

    Update testingDimensions.java

--- CheckingDimensions/testingDimensions.java
+++ CheckingDimensions/testingDimensions.java
@@ -1,60 +1,60 @@
-public class UnitConverterTest {
+public class CheckingDimensionsTest{
 	
-	UnitConverter unitConverter = new UnitConverter();
+	CheckingDimensions checkingDimensions = new CheckingDimensions();
 	
 
 	@Test
 	public void testCentimterToCentimeter() {
-		Assert.assertEquals(1,unitConverter.convertCentimeterToCentimeter(1));
+		Assert.assertEquals(1,checkingDimensions.convertCentimeterToCentimeter(1));
 	}
 	
 	@Test
 	public void testMeterToCentimeter() {
-		Assert.assertEquals(100,unitConverter.convertMeterToCentimeter(1));
+		Assert.assertEquals(100,checkingDimensions.convertMeterToCentimeter(1));
 	}
 	
 	@Test
 	public void testCentimeterToKilometer() {
-		Assert.assertEquals("0.001",unitConverter.convertCentimeterToKilomter(100));
+		Assert.assertEquals("0.001",checkingDimensions.convertCentimeterToKilomter(100));
 	}
 	
 	@Test
 	public void testAddMeterWithCentimeterAndConvertToMeter() {
-		Assert.assertEquals(2,unitConverter.addMeterWithCentimeterAndConvertToMeter(1,100));
+		Assert.assertEquals(2,checkingDimensions.addMeterWithCentimeterAndConvertToMeter(1,100));
 	}
 	
 	@Test
 	public void testAddCentimeterWithKilometerAndConvertToCentimeter() {
-		Assert.assertEquals(100200,unitConverter.addCentimeterWithKilometerAndConvertToCentimeter(200,1));
+		Assert.assertEquals(100200,checkingDimensions.addCentimeterWithKilometerAndConvertToCentimeter(200,1));
 	}
 	
 	@Test
 	public void testConvertGramToGram() {
-		Assert.assertEquals(1,unitConverter.convertGramToGram(1));
+		Assert.assertEquals(1,checkingDimensions.convertGramToGram(1));
 	}
 	
 	@Test
 	public void testConvertKilogramToGram() {
-		Assert.assertEquals(100,unitConverter.convertKilogramToGram(0.1));
+		Assert.assertEquals(100,checkingDimensions.convertKilogramToGram(0.1));
 	}
 	
 	@Test
 	public void testAddGramWithKilogramAndConvertToGram() {
-		Assert.assertEquals(1010,unitConverter.addGramWithKilogramAndConvertToGram(10, 1));
+		Assert.assertEquals(1010,checkingDimensions.addGramWithKilogramAndConvertToGram(10, 1));
 	}
 	
 	@Test
 	public void testSubractGramFromKilogramAndConvertToKilogram() {
-		Assert.assertEquals(1,unitConverter.subractGramFromKilogramAndConvertToKilogram(1.5,500));
+		Assert.assertEquals(1,checkingDimensions.subractGramFromKilogramAndConvertToKilogram(1.5,500));
 	}
 	
 	@Test
 	public void testConvertCelsiusToFahrenheit() {
-		Assert.assertEquals(32,unitConverter.convertCelsiusToFahrenheit(0));
+		Assert.assertEquals(32,checkingDimensions.convertCelsiusToFahrenheit(0));
 	}
 	
 	@Test
 	public void testConvertKelvinToCelsius() {
-		Assert.assertEquals(-273,unitConverter.convertKelvinToCelsius(0));
+		Assert.assertEquals(-273,checkingDimensions.convertKelvinToCelsius(0));
 	}
 }
